0db089db2d2d6880bc59d5b69a17417e9c66f767
slavamorozov <morozikchetkiu2@gmail.com>
2024-07-14 13:28:48 +0300

    added clear site

---
+++ src/components/Header/header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header>
+            <div className='navbar'>
+                <div className='logo'></div>
+                <ul className= 'nav'>
+                    <li>О нас</li>
+                    <li>Контакты</li>
+                    <li>Купить пиво</li>
+                </ul>
+            </div>
+            <div className='presentation'></div>
+        </header>
+    )
+}
